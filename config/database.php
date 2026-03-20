2bf55abe465a0f4a51048cf9d22f2aac507293ff
Diana Benavides <dcbenavides.6899@unicesmag.edu.co>
2026-03-19 19:51:35 -0500

    repositor

--- config/database.php
+++ config/database.php
@@ -3,26 +3,47 @@
 $LOCA_HOST='localhost';//127.0.0.1
 $LOCAL_DBNAME='app_beta';
 $LOCAL_USERNAME='postgres';
-$LOCAL_PASSWORD='unicesmag';
-$LOCAOL_PORT='5432';
+$LOCAL_PASSWORD='benavides';
+$LOCAL_PORT='5432';
 //supabase database configuration
-$SUPA_HOST='';
-$SUPA_DBNAME='';
-$SUPA_USERNAME='';
-$SUPA_PASSWORD='';
-$SUPA_PORT='';
-$data_connection="
+$SUPA_HOST='aws-1-us-east-2.pooler.supabase.com';
+$SUPA_DBNAME='postgres';
+$SUPA_USERNAME='postgres.qjiciljryzzgzjfcxzqs';
+$SUPA_PASSWORD='unicesmag@@';
+$SUPA_PORT='6543';
+
+
+
+$local_data_connection="
 host=$LOCA_HOST
 dbname=$LOCAL_DBNAME
 user=$LOCAL_USERNAME
 password=$LOCAL_PASSWORD
-port=$LOCAOL_PORT
+port=$LOCAL_PORT
 ";
-$conn=pg_connect($data_connection);
-if(!$conn){
-    echo"Error:UNABLE TO CONNECT TO DATABASE";
+$supa_data_connection="
+host=$SUPA_HOST
+dbname=$SUPA_DBNAME
+user=$SUPA_USERNAME
+password=$SUPA_PASSWORD
+port=$SUPA_PORT
+";
+
+$local_conn=pg_connect($local_data_connection);
+if(!$local_conn){
+    echo"Error:UNABLE TO CONNECT TO LOCAL DATABASE";
     exit();
 }else{
-    echo"Success connection!!!";
+    echo"Local success connection!!!";
 }
+
+
+$supa_conn=pg_connect($supa_data_connection);
+if(!$supa_conn){
+    echo"Error:UNABLE TO CONNECT TO SUPABASE DATABASE";
+    exit();
+}else{
+    echo"<br>Supabase success connection!!!";
+}
+
 ?>
