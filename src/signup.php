a6904c28756f3ecb0cdd4c155bc4fe58becbc81f
Diana Benavides <dcbenavides.6899@unicesmag.edu.co>
2026-03-27 11:00:23 -0500

    signup.html git commit -m  signup.html git commit -m  signup.html

--- src/signup.php
+++ src/signup.php
@@ -5,7 +5,8 @@ $l_name = $_POST  ['lname'];
 $e_mail = $_POST  ['email'];
 $m_phone = $_POST  ['mphone'];
 $p_sswd = $_POST  ['passwd'];
+$enc_pass=md5($p_sswd);
 $sql = "INSERT INTO users (firstname,lastname,email,mobile_phone,password,password_again) 
-values('$f_name','$l_name','$e_mail','$m_phone','$p_sswd','$p_sswd')";
+values('$f_name','$l_name','$e_mail','$m_phone','$p_sswd','$enc_pass')";
 //execute query
 pg_query ($sql);
